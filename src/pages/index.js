e401bcbf51a538c1f62b6d94cbededef594175e0
Leila Suleimanova <leila_suleymanova@mail.ru>
2023-11-29 04:55:10 +0300

    Добавляет прелоадер и сообщение с ошибками

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,13 @@
 import '../pages/index.css'
 
-//Получаем ссылку на форму и элементы ввода
 let form = document.querySelector('.form__inputs'),
   formInputs = document.querySelectorAll('.form__input'),
   inputEmailNumber = document.querySelector('.form__input-email'),
-  inputPassword = document.querySelector('.form__input-password')
+  inputPassword = document.querySelector('.form__input-password'),
+  successText = document.querySelector('.success__text'),
+  preloader = document.querySelector('.preloader');
 
-//Определяем функцию для проверки валидности email или номера телефона
+// Функция валидации email или номера телефона
 function validateEmailNumber(inputVal) {
   let reEmail = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
   let reNumber = /^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10,14}(\s*)?$/;
@@ -14,15 +15,12 @@ function validateEmailNumber(inputVal) {
 }
 
 // Обработчик отправки формы
-form.onsubmit = function (event) {
-  // Отменяем действие по умолчанию для формы (отправку)
-  event.preventDefault();
+form.onsubmit = function (e) {
+  e.preventDefault(); // Отменить отправку формы по умолчанию
 
-  // Получаем значения полей
-  let emailNumberVal = inputEmailNumber.value;
+  let emailNumberVal = inputEmailNumber.value.trim();
   let emptyInputs = Array.from(formInputs).filter(input => input.value === '');
 
-  // Удаляем класс 'error' у всех полей формы
   formInputs.forEach(function (input) {
     input.classList.remove('error');
   });
@@ -33,54 +31,61 @@ form.onsubmit = function (event) {
     emptyInputs.forEach(function (input) {
       input.classList.add('error');
     });
+    document.querySelector('.form__error').innerText = 'Поля формы не заполены';
+    document.querySelector('.form__error').style.display = "block";
   } else {
     // Если все поля заполнены, проверяем валидность email или номера телефона
     if (!validateEmailNumber(emailNumberVal)) {
-      // Если email или номер телефона некорректны, добавляем класс 'error' к полю email/номера телефона
+      // Если email или номер телефона некорректны, добавляем классы ошибок
       inputEmailNumber.classList.add('error');
-      console.log('not valid');
+      document.querySelector('.form__error').innerText = 'Неправильный логин или пароль';
+      document.querySelector('.form__error').style.display = "block";
+
     } else {
-      // Если все введено корректно, продолжаем с авторизацией
       authorize();
     }
   }
 }
 
-// Функция для авторизации
 function authorize() {
-  // Отправляем запрос на сервер для авторизации
+
+  // Отправить запрос на сервер
+  // (API не рабочий или данные логина/пароля недостоверные?)
   fetch('https://test-works.pr-uni.ru/api/login', {
     method: 'POST',
-    credentials: 'include', // Включаем отправку куки
     headers: {
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({
-      emailNumber: inputEmailNumber.value,
+      email_number: emailNumberVal.value,
       password: inputPassword.value
     })
   })
-    .then(response => response.json())
-    .then(data => {
-      // Проверяем ответ от сервера
-      if (data.success) {
-        // Если авторизация прошла успешно, сохраняем токен в куки
-        document.cookie = `token = ${data.token}`;
-        // Скрываем форму и выводим сообщение об успешной авторизации
-        form.style.display = 'none';
-        let successMessage = document.createElement('p');
-        successMessage.textContent = `${data.user.name}, Вы успешно авторизованы!`;
-        form.parentNode.appendChild(successMessage);
+    .then(response => {
+      // Скрыть прелоадер и разблокировать форму
+      preloader.style.display = 'none';
+      form.classList.remove('disabled');
+
+      if (response.ok) {
+        return response.json();
       } else {
-        // Если авторизация не удалась, отображаем ошибку
-        alert('Ошибка авторизации. Попробуйте еще раз.');
+        throw new Error('Ошибка при авторизации');
       }
     })
+    .then(data => {
+      // Сохранить токен в куки
+      document.cookie = `token = ${data.token}`;
+
+      // Скрыть форму и показать текст успеха
+      form.style.display = 'none';
+      successText.innerText = `${data.user.name}, Вы успешно авторизованы!`;
+      document.querySelector('.success').style.display = 'block';
+    })
     .catch(error => {
-      // Если произошла ошибка при отправке запроса или получении ответа, отображаем ошибку
-      alert('Произошла ошибка. Попробуйте еще раз.');
+      // Обработка ошибок
       console.error(error);
+      // Вывести ошибку в интерфейсе
+      document.querySelector('.form__error').innerText = 'Ошибка при авторизации';
     });
 }
-
 
